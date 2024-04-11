b5e5696b4f05ffa91312c8464f2abc2f0c433675
Kira Pitman <kirapitman@gmail.com>
2024-04-12 09:45:57 +1200

    update on collision enter 2D function for wall jumping and future

--- playerMovement.cs
+++ playerMovement.cs
@@ -94,10 +94,8 @@ public class playerMovement : MonoBehaviour
  }
 
      private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
-    }
+{
+}
 
     private bool isGrounded()
 {
