1d07f27c52907b2d7b92ae2170c22526a006e7a8
Kira Pitman <kirapitman@gmail.com>
2024-04-11 16:11:12 +1200

    write code for jumping

--- playerMovement.cs
+++ playerMovement.cs
@@ -4,10 +4,14 @@ public class playerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+        // get references for rigidbody and animator from game object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -30,6 +34,10 @@ public class playerMovement : MonoBehaviour
         
         if (Input.GetKey(KeyCode.Space) && grounded) // only allows jumping when on the ground
             Jump();
+
+        anim.SetBool("run", horizontalInput != 0); // ie if keys aren't pressed then false, if they are then true!
+        anim.SetBool("grounded", grounded);
+        // sets animator parameters i.e. for transitions
     }
 
     private void Jump()
@@ -38,5 +46,11 @@ public class playerMovement : MonoBehaviour
         anim.SetTrigger("jump");
         grounded = false;
         // to jump using space bar
+    }
+
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
