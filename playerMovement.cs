3addf63c6bb285061bbba6fdbcb84e15bc4ee955
Kira Pitman <kirapitman@gmail.com>
2024-04-11 16:08:47 +1200

    write code for player to jump

--- playerMovement.cs
+++ playerMovement.cs
@@ -19,5 +19,15 @@ public class playerMovement : MonoBehaviour
             body.velocity = new Vector2(body.velocity.x, speed);
         // to jump using space bar
         
+        if (Input.GetKey(KeyCode.Space) && grounded) // only allows jumping when on the ground
+            Jump();
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+        // to jump using space bar
     }
 }
