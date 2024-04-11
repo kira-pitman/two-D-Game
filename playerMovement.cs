13ec97abd5a5f573fad8f84657f7adf097586291
Kira Pitman <kirapitman@gmail.com>
2024-04-11 16:09:44 +1200

    write code to change direction player faces as it moves

--- playerMovement.cs
+++ playerMovement.cs
@@ -12,8 +12,17 @@ public class playerMovement : MonoBehaviour
 
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
         // lets us use arrow keys for movement
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        // faces right if moving right
+
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+        // faces left if moving left
 
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
