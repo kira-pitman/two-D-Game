b78acc5e66e4f0d86f46f9642b1c4b218b709f56
Kira Pitman <kirapitman@gmail.com>
2024-04-16 21:34:27 +1200

    write initial fireball setup

---
+++ projectile.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]private float speed;
+    private bool hit;
+
+    private Animator anim;
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (hit) return;
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(movementSpeed, 0, 0);
+    }
+}
