c1d3754b9cdc577f5884c76b39275e26f98593ce
Kira Pitman <kirapitman@gmail.com>
2024-04-16 20:55:19 +1200

    write initial attack code

---
+++ playerAttack.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class playerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown; // time between attacks
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<playerMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown)
+            Attack();
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
+    }
+
+}
