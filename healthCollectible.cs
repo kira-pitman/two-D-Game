c7aab936bc2e964139c5836ce393814df7f8c763
Kira Pitman <kirapitman@gmail.com>
2024-04-29 19:34:50 +1200

    serialize health value

--- healthCollectible.cs
+++ healthCollectible.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    private float healthValue;
+    [SerializeField] private float healthValue;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
