e983d1df02ea16ce56ce8168c3fb225962fd4cc0
Kira Pitman <kirapitman@gmail.com>
2024-05-08 14:09:29 +1200

    fix formatting attempt 1

---
+++ Projectile.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float direction;
+    private bool hit;
+    private float lifetime; // how many seconds projectile has been active
+
+    private Animator anim;
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (hit) return;
+        float movementSpeed = speed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0, 0);
+
+        lifetime += Time.deltaTime;
+        if(lifetime > 5) gameObject.SetActive(false); 
+        // makes sure if fireball doesn't hit anything it won't stop it from deactivating
+    }
+
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("explode");
+    }
+
+    public void SetDirection(float _direction)
+    {
+        lifetime = 0;
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
+            // make fireball change direction when fired
+    
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
